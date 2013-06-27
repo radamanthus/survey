1feb90af35793015401f315b694739061e7d9478
radamanthus <rad@batnag.org>
2013-06-27 09:14:31 +0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Survey::Application.config.secret_key_base = 'b1500a5ac67be198fe2af125ef1e2e8d38d5ff1fd8fdd21edcb9b5b09c7da934fb4bfa82507ff8ec38392446127cc78bcd4900ad13babada7d8a405e44f5854f'
