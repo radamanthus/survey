756bda529d155c222612d5ce0732578058e30653
Radamanthus Batnag <rad@batnag.org>
2013-09-03 10:40:06 +0800

    add intentionally failing health check

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,4 +2,8 @@ class HomeController < ApplicationController
   def index
     render text: 'Hello, world!'
   end
+
+  def health
+    raise 'oh-oh'
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Survey::Application.routes.draw do
   # You can have the root of your site routed with "root"
   root 'home#index'
 
+  get '/health' => 'home#health'
+
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
